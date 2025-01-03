65cc3ee2d44686214c28829f91ae1f86e50eb9dc
Edwin Peña <edwinsp095@gmail.com>
2025-01-02 23:30:20 -0500

    Move 'components' folder to 'src' (was mistakenly in 'assets')

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ const App: React.FC = () => {
   return (
     <div className="page">
       <SearchBar />
+      {/* here goes the component background-movie */}
 
       <div className="movie-bg">
         <h1 className="movie-bg-title">RAYA AND THE LAST DRAGON</h1>
